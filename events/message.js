826b2c5c13cfc90e4cf983866e05ac0ed83b576b
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-05-10 01:41:49 +0300

    Handles mentions now

--- events/message.js
+++ events/message.js
@@ -3,6 +3,10 @@
 // goes `client, other, args` when this function is run.
 
 module.exports = (client, message) => {
+  var parsedContent = message.content;
+  if (message.isMentioned(client.user)) {
+    parsedContent = message.content.split(" ")[1]
+  }
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
   if (message.author.bot) return;
@@ -13,13 +17,13 @@ module.exports = (client, message) => {
 
   // Also good practice to ignore any message that does not start with our prefix,
   // which is set in the configuration file.
-  if (message.content.indexOf(settings.prefix) !== 0) return;
+  if (parsedContent.indexOf(settings.prefix) !== 0) return;
 
   // Here we separate our "command" name, and our "arguments" for the command.
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
+  const args = parsedContent.slice(settings.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   // Get the user or member's permission level from the elevation
