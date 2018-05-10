6b0d56a5bb7a2b72224e45db03e8b4c70dfc121e
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-05-10 16:25:57 +0300

    fix pleb

--- commands/pleb.js
+++ commands/pleb.js
@@ -1,18 +1,21 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-    const msg = await message.channel.send("", {files: "https://imgur.com/TRQ3rtA"});
-  };
-  
-  exports.conf = {
+    const msg = await message.channel.send(
+        "Kasuarbird", {
+            file: "https://imgur.com/TRQ3rtA"
+        }
+    );
+};
+
+exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
     permLevel: "User"
-  };
-  
-  exports.help = {
+};
+
+exports.help = {
     name: "pleb",
     category: "Misc",
     description: "...",
     usage: "pleb"
-  };
-  
+};
