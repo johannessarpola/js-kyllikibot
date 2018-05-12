bbbce75e557755ffd70d5682e275778deabcb59c
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-05-12 23:07:46 +0300

    Upcoming should not be guild only

--- commands/upcoming.js
+++ commands/upcoming.js
@@ -12,7 +12,7 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
 
 exports.conf = {
     enabled: true,
-    guildOnly: true,
+    guildOnly: false,
     aliases: [],
     permLevel: "User"
 };
