2122b176143cec42a2d1103dd4782be35570ed47
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-05-12 23:18:02 +0300

    Some formatting

--- commands/upcoming.js
+++ commands/upcoming.js
@@ -1,10 +1,16 @@
+const moment = require('moment');
+const formats = require('../configs/timeFormats.json');
+const dateTimeFormat = formats.generalFormat;
+const timeFormat = formats.timeFormat;
+
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     const gcal = require('../services/gcalendar')
     const upcoming = gcal.upcoming((event => {
         // TODO Handle date format as well
         // FIXME Handle no events
-        const start = event.start.dateTime || event.start.date;
-        let evt = `${start} - ${event.summary} (${event.id})`
+        const startMoment =  moment(event.start.dateTime || event.start.date);
+        const endMoment = moment(event.end.dateTime || event.start.date)
+        let evt = `${startMoment.format(dateTimeFormat)} - ${endMoment.format(timeFormat)} : ${event.summary} (${event.id})`
         message.channel.send(evt)}
     ))
     const msg = await message.channel.send("Fetching events ...");
