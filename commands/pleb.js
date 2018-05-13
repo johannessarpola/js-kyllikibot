980ffcb4fb7cd2af5268e11930dfd02712d5032a
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-05-13 16:44:35 +0300

    Mention in pleb

--- commands/pleb.js
+++ commands/pleb.js
@@ -1,6 +1,7 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+    let user  = message.guild.members.find("displayName", args.join(" "))
     const msg = await message.channel.send(
-        "Kasuarbird", {
+        user != undefined ? `${user}` : "Kasuarbird", {
             file: "https://i.imgur.com/TRQ3rtA.jpg"
         }
     );
